3c9ec2e664f00403c6a6d15a475832591a609cbb
Bego Blanco <begoblanfer@gmail.com>
2024-06-09 14:08:21 +0200

    changes

---
+++ src/part1/app.js
@@ -0,0 +1,14 @@
+
+const path = "/src/data.json"
+
+async function getData(){
+    const response = await fetch (path)
+    const data = await response.json()
+
+    for (let index = 0; index < data.results.length; index++) {
+        console.log(data.results[index].title);
+        
+    }
+}
+
+getData();
